ca0da5bea585518ae584a906c3b8fd8cf69809b7
lax3331 <la2life1234@gmail.com>
2023-09-16 16:51:19 +0400

    Initial Commit

---
+++ Your Train/Your_TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_TrainApp.swift
+//  Your Train
+//
+//  Created by Nikita Kuznetsov on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Your_TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
